114dae38abc00d7315d238cd8997eeb52f12b2ce
Jackson Hong <jacksonhong0111@gmail.com>
2020-12-28 20:23:42 +0900

    baekjoon #1110

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,26 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int input = sc.nextInt();
+        System.out.println(plusCycle(input));
+    }
+    // baekjoon #1110
+    static int plusCycle(int num){
+        int result = 0;
+        int idx = 0;
 
+         do{
+            int input = 0;
+
+            if(idx==0) input = num;
+            else input = result;
+
+            result = (input%10) * 10 + (input/10+input%10) % 10;
+
+            idx++;
+        }while(result!=num);
+
+        return idx;
     }
 }
