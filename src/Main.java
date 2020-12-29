ba29ffdb19fde4fd4b1127f6c2a249bd0f44c568
Jackson Hong <jacksonhong0111@gmail.com>
2020-12-29 22:03:50 +0900

    baekjoon #2675

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,24 @@ import java.util.Scanner;
 public class Main {
     static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-        searchAlphabet(sc.next());
+        duplicateString(sc.nextInt());
     }
-    // baekjoon #10809
-    static void searchAlphabet(String str){
-
-        for(int i = 97; i < 123; i++){
-            CharSequence chaa = ""+(char)i;
-            if(str.contains(chaa))System.out.print(str.indexOf(i)+" ");
-            else System.out.print(-1 + " ");
+    // baekjoon #2675
+    static void duplicateString(int num){
+        sc.nextLine();
+        for(int i = 0; i < num; i++){
+            //System.out.println("loop start");
+            String wholeInput = sc.nextLine();
+            int dupCount = Character.getNumericValue(wholeInput.charAt(0));
+            //System.out.println("dupCount passed:"+dupCount);
+            String input = wholeInput.substring(2,wholeInput.length());
+            //System.out.println("input passed:"+input);
+            for(int j = 0; j < input.length(); j++){
+                for(int k = 0; k < dupCount; k++){
+                    System.out.print(input.charAt(j));
+                }
+            }
+            System.out.println();
         }
     }
 }
