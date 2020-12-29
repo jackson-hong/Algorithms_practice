6027b8eb8bed7ed0b73602d23ecd207b76b410f4
Jackson Hong <jacksonhong0111@gmail.com>
2020-12-29 19:40:31 +0900

    baekjoon #11654

--- src/Main.java
+++ src/Main.java
@@ -3,39 +3,13 @@ import java.util.Scanner;
 public class Main {
     static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-        System.out.println(hansoo(sc.nextInt()));
+        System.out.println(asciiCode(sc.next().charAt(0)));
     }
-    // baekjoon #1065
-    static int hansoo(int num){
-
+    // baekjoon #11654
+    static int asciiCode(char num){
         int result = 0;
 
-        for(int i = 1; i <= num; i++){
-            if(i<100)result++;
-            else {
-                int numCount = 0;
-                int[] nums = new int[5];
-                int temp = i;
-                while(temp!=0){
-                    nums[numCount] = temp%10;
-                    temp /= 10;
-                    numCount++;
-                }
-                int gap = 0;
-                for(int j = 0; j < numCount-1; j++){
-                    if(j==0)
-                        gap = nums[j] - nums[j+1];
-                    else {
-                        if(gap!=nums[j] - nums[j+1])break;
-                        else{
-                            if(j==numCount-2){
-                                result++;
-                            }
-                        }
-                    }
-                }
-            }
-        }
+        result = num;
 
         return result;
     }
