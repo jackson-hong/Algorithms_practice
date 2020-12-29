a4818f43a6a5c8b3dd5f015c94266f59d8144e94
Jackson Hong <jacksonhong0111@gmail.com>
2020-12-29 19:30:49 +0900

    beakjoon #1065

--- src/Main.java
+++ src/Main.java
@@ -3,26 +3,40 @@ import java.util.Scanner;
 public class Main {
     static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-        boolean[] nums = new boolean[10000];
-        for(int i = 1; i < 10000; i++){
-            int num = i;
-            int temp = 0;
-            while(num < 10000){
-                int plus = 0;
-                temp = num;
-                while(temp > 0){
-                    plus += temp%10;
+        System.out.println(hansoo(sc.nextInt()));
+    }
+    // baekjoon #1065
+    static int hansoo(int num){
+
+        int result = 0;
+
+        for(int i = 1; i <= num; i++){
+            if(i<100)result++;
+            else {
+                int numCount = 0;
+                int[] nums = new int[5];
+                int temp = i;
+                while(temp!=0){
+                    nums[numCount] = temp%10;
                     temp /= 10;
+                    numCount++;
                 }
-                num += plus;
-                if(num < 10000)nums[num] = true;
+                int gap = 0;
+                for(int j = 0; j < numCount-1; j++){
+                    if(j==0)
+                        gap = nums[j] - nums[j+1];
+                    else {
+                        if(gap!=nums[j] - nums[j+1])break;
+                        else{
+                            if(j==numCount-2){
+                                result++;
+                            }
+                        }
+                    }
+                }
             }
         }
-        for(int i = 1; i < nums.length; i++){
-            if(nums[i]==false)System.out.println(i);
-        }
-    }
-    // baekjoon #4673
-    static void selfNumber(){
+
+        return result;
     }
 }
