b3a857ccd98c84b520e6252da937299fb09b9fc3
Jackson Hong <jacksonhong0111@gmail.com>
2021-01-02 18:08:05 +0900

    baekjoon #5622

--- src/Main.java
+++ src/Main.java
@@ -8,13 +8,30 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
-        int a = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
-        int b = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
-
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        String str = st.nextToken();
+        int result = 0;
+        char[][] ch = {{},{},{},{'A','B','C'},{'D','E','F'},{'G','H','I'},{'J','K','L'},{'M','N','O'}
+                ,{'P','Q','R','S'},{'T','U','V'},{'W','X','Y','Z'}};
+        for(int i = 0; i < str.length(); i++){
+            char c = str.charAt(i);
+            for(int k = 0; k < ch.length; k++){
+                if(ch[k].length!=0){
+                    for(int j = 0; j < ch[k].length; j++){
+                        if(ch[k][j]==c){
+                            result += k;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        System.out.println(result);
     }
-    // baekjoon #2908
+    // baekjoon #5622
     static void solution(String str){
     }
+    //A = 65
+
+    //65, 66, 67 = 3,
 }
