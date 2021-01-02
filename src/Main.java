54651ecfde55629bc33b5c82093a91f9fcdb1145
Jackson Hong <jacksonhong0111@gmail.com>
2021-01-02 19:00:31 +0900

    baekjoon #2941

--- src/Main.java
+++ src/Main.java
@@ -6,32 +6,25 @@ import java.util.*;
 public class Main {
     static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        String input = st.nextToken();
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        String str = st.nextToken();
+        String[] cr = {"dz=","c=","c-","d-","lj","nj","s=","z="};
         int result = 0;
-        char[][] ch = {{},{},{},{'A','B','C'},{'D','E','F'},{'G','H','I'},{'J','K','L'},{'M','N','O'}
-                ,{'P','Q','R','S'},{'T','U','V'},{'W','X','Y','Z'}};
-        for(int i = 0; i < str.length(); i++){
-            char c = str.charAt(i);
-            for(int k = 0; k < ch.length; k++){
-                if(ch[k].length!=0){
-                    for(int j = 0; j < ch[k].length; j++){
-                        if(ch[k][j]==c){
-                            result += k;
-                            break;
-                        }
-                    }
-                }
+        for(int i = 0; i < cr.length; i++){
+            while(input.contains(cr[i])){
+                result++;
+                input = input.replaceFirst(cr[i]," ");
             }
         }
-        System.out.println(result);
+        input.startsWith("");
+        input = input.replaceAll(" ","");
+        System.out.println(result+input.length());
     }
     // baekjoon #5622
+    // baekjoon #2941
     static void solution(String str){
     }
-    //A = 65
 
-    //65, 66, 67 = 3,
 }
