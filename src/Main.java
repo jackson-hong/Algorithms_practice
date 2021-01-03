e63ec7456a6d8f29cf62b2728693621d769df45b
Jackson Hong <jacksonhong0111@gmail.com>
2021-01-03 17:28:42 +0900

    baekjoon #1316

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,41 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.lang.reflect.Array;
 import java.util.*;
 
 public class Main {
     static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) throws IOException {
-        solution(sc.nextInt());
-    }
-    // programmers findPrimeNum
-    static int solution(int n){
-        ArrayList<Integer> list = new ArrayList();
-        list.add(2);
-        if(n > 2) {
-            for (int i = 3; i <= n; i++) {
-                for (int j = 0; j < list.size(); j++) {
-                    if (i % list.get(j) != 0) {
-                        if (j == list.size() - 1) {
-                            list.add(i);
-                            break;
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        int input = Integer.parseInt(st.nextToken());
+        int result = 0;
+        for(int i = 0; i < input; i++){//인풋을 몇 번 받을
+            String str = bf.readLine();
+            ArrayList<Character> chList = new ArrayList();
+            int idx = 0;
+            outerloop:
+            for(int j = 0; j < str.length(); j++){ //인풋의 0번째 자리수 부터 확인
+                if(j==0){
+                    chList.add(str.charAt(j));
+                } //0번째 자리인 경우 확인 리스트에 넣음
+                else {
+                    if(str.charAt(j)!=chList.get(idx)){
+                        if(chList.contains(str.charAt(j))){
+                            break outerloop;
+                        }else {
+                            chList.add(str.charAt(j));
+                            idx++;
                         }
-                    }else {
-                        break;
                     }
                 }
+                if(j==str.length()-1)result++;
             }
         }
-        return list.size();
+        System.out.println(result);
     }
-
+    // baekjoon #1316
+    static void solution(int input){
+    }
 }
