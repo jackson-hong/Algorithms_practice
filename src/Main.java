5bab7279ca32af4415fdb46a88bbed51154ab392
Jackson Hong <jacksonhong0111@gmail.com>
2021-01-07 20:18:16 +0900

    baekjoon #10250

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,26 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    //baekjoon #2869
+    //baekjoon #10250
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(bf.readLine()," ");
-        int day = Integer.parseInt(st.nextToken());
-        int night = Integer.parseInt(st.nextToken());
-        int v = Integer.parseInt(st.nextToken());
-
-        System.out.println((int)Math.ceil((v-night)/(double)(day-night)));
-
+        int times = Integer.parseInt(bf.readLine());
+        for(int i = 0; i < times; i++){
+            StringTokenizer st = new StringTokenizer(bf.readLine()," ");
+            int h = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+            int customer = Integer.parseInt(st.nextToken());
+            int idx = 0;
+            int j = 0;
+            int k = 0;
+            outerloop:
+            for(j = 1; j <= w; j++){
+                for(k = 1; k <= h; k++){
+                    idx++;
+                    if(idx==customer)break outerloop;
+                }
+            }
+            System.out.printf("%d%02d\n",k,j);
+        }
     }
 }
