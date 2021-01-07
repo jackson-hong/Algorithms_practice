8a9f4ff8734e415b4bb883b1ddc2a2713057a2e6
Jackson Hong <jacksonhong0111@gmail.com>
2021-01-07 19:33:16 +0900

    baekjoon #2869

--- src/Main.java
+++ src/Main.java
@@ -4,21 +4,15 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    //baekjoon #1193
+    //baekjoon #2869
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        int input = Integer.parseInt(bf.readLine());
-        int idx = 0;
-        int i;
-        int j;
-        outerloop:
-        for(i = 1; ; i++){
-            for(j = 1; j <= i; j++){
-                idx++;
-                if(idx == input)break outerloop;
-            }
-        }
-        if(i%2==1)System.out.println(""+(i-j+1)+"/"+j);
-        else System.out.println(""+j+"/"+(i-j+1));
+        StringTokenizer st = new StringTokenizer(bf.readLine()," ");
+        int day = Integer.parseInt(st.nextToken());
+        int night = Integer.parseInt(st.nextToken());
+        int v = Integer.parseInt(st.nextToken());
+
+        System.out.println((int)Math.ceil((v-night)/(double)(day-night)));
+
     }
 }
