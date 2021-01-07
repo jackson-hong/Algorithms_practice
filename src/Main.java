bf37c86da47380ee425e5fefece4a5e3d6250c56
Jackson Hong <jacksonhong0111@gmail.com>
2021-01-07 19:07:13 +0900

    baekjoon #1193

--- src/Main.java
+++ src/Main.java
@@ -4,21 +4,21 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
+    //baekjoon #1193
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
-        long result = 0;
-        long base = Long.parseLong(st.nextToken());
-        long plus = Long.parseLong(st.nextToken());
-        long margin = Long.parseLong(st.nextToken());
-        if(plus >= margin){
-            System.out.println(-1);
-            return;
+        int input = Integer.parseInt(bf.readLine());
+        int idx = 0;
+        int i;
+        int j;
+        outerloop:
+        for(i = 1; ; i++){
+            for(j = 1; j <= i; j++){
+                idx++;
+                if(idx == input)break outerloop;
+            }
         }
-
-        result = base/(margin-plus)+1;
-
-        System.out.println(result);
+        if(i%2==1)System.out.println(""+(i-j+1)+"/"+j);
+        else System.out.println(""+j+"/"+(i-j+1));
     }
-    // baekjoon #1712
 }
