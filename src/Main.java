4c1d3937614928211359a8b522453dbb7f815d9a
Jackson Hong <jacksonhong0111@gmail.com>
2021-01-08 16:34:23 +0900

    baekjoon #2775 5 attempts...

--- src/Main.java
+++ src/Main.java
@@ -4,26 +4,26 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    //baekjoon #10250
+    //baekjoon #2775
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         int times = Integer.parseInt(bf.readLine());
-        for(int i = 0; i < times; i++){
-            StringTokenizer st = new StringTokenizer(bf.readLine()," ");
-            int h = Integer.parseInt(st.nextToken());
-            int w = Integer.parseInt(st.nextToken());
-            int customer = Integer.parseInt(st.nextToken());
-            int idx = 0;
-            int j = 0;
-            int k = 0;
-            outerloop:
-            for(j = 1; j <= w; j++){
-                for(k = 1; k <= h; k++){
-                    idx++;
-                    if(idx==customer)break outerloop;
+        int[][] nums = new int[15][15];
+        for(int j = 0; j < nums.length;j++){
+            nums[0][j] = j+1;
+        }
+        for(int j = 1; j < nums.length; j++){
+            for(int k = 0; k < nums[j].length; k++){
+                if(k == 0)nums[j][k]=1;
+                else {
+                    nums[j][k] = nums[j][k-1] + nums[j-1][k];
                 }
             }
-            System.out.printf("%d%02d\n",k,j);
+        }
+        for(int i = 0; i < times; i++){
+            int h = Integer.parseInt(bf.readLine());
+            int w = Integer.parseInt(bf.readLine());
+            System.out.println(nums[h][w-1]);
         }
     }
 }
