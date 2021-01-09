c3e6eb18a18ec8a70a5e09ab72b822d18f96e9f3
Jackson Hong <jacksonhong0111@gmail.com>
2021-01-09 21:20:23 +0900

    baekjoon #1011

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,30 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigDecimal;
 import java.util.*;
 
 public class Main {
-    //baekjoon #2839
+    //baekjoon #1011
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        int input = Integer.parseInt(bf.readLine());
-        int num = 1001;
-        int idx = 1;
-        while(num!=0){
-            if(input%5==0){//5로 나뉘는 경우
-                num = input / 5;
-                break;
-            }else{//5로 나뉘지 않는 경우
-                if(input%5==3){//5로 나눈 나머지가 3일 경우
-                    num = input / 5;
-                    num += 1;
+        int times = Integer.parseInt(bf.readLine());
+        for(int i = 0; i < times; i++){
+            StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
+            int start = Integer.parseInt(st.nextToken());
+            int goal = Integer.parseInt(st.nextToken());
+            int distance = goal - start;
+            int idx= 1;
+
+            for(;;idx++){
+                if(distance < Math.pow(idx,2)){
+                    idx--;
                     break;
-                }else {//5로 나눈 나머지가 3이 아닐 경우
-                    num = input/5 - idx;
-                    if(num<0){
-                        num = -1;
-                        break;
-                    }
-                    int rest = input - num*5;
-                    idx++;
-                    if(rest%3==0){
-                        num += rest/3;
-                        break;
-                    }
+                }else if(distance == Math.pow(idx,2)){
+                    break;
                 }
             }
-            if(num<0){
-                num = -1;
-                break;
-            }
-            num = 1001;
+            System.out.println((int)Math.ceil(2*idx-1 + (distance-Math.pow(idx,2))/idx));
         }
-        System.out.println(num);
     }
 }
