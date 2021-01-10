a04105e241e6df96d3ed75d2c646b3b369518e74
Jackson Hong <jacksonhong0111@gmail.com>
2021-01-10 17:20:15 +0900

    baekjoon #1929

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ import java.math.BigDecimal;
 import java.util.*;
 
 public class Main {
-    //baekjoon #11653
+    //baekjoon #1929
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         int input = Integer.parseInt(bf.readLine());
