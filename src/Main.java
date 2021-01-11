922431dcbeadd394b2fe9d04cd442c65c1d72334
Jackson Hong <jacksonhong0111@gmail.com>
2021-01-11 23:21:41 +0900

    baekjoon #9020

--- src/Main.java
+++ src/Main.java
@@ -5,24 +5,42 @@ import java.math.BigDecimal;
 import java.util.*;
 
 public class Main {
-    //baekjoon #1929
+    //baekjoon #9020
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(bf.readLine()," ");
-        int start = Integer.parseInt(st.nextToken());
-        int end = Integer.parseInt(st.nextToken());
-        boolean[] table = new boolean[end+1];//true = isNotPrime false = isPrime
+        int max = 10000;
+        boolean[] table =  new boolean[max+1]; //false = isPrime
+        ArrayList<Integer> primes = new ArrayList<>();
         table[0] = table[1] = true;
-        for(int i = 2; i*i < table.length; i++){
+        for(int i = 2; i*i<max+1; i++){
             if(!table[i]){
-                for(int j = i*i; j < table.length; j += i){
-                    table[j] = true;
+                for(int j = i*i; j < max+1; j+=i){
+                    table[j]=true;
                 }
             }
         }
-
-        for(int i = start; i < table.length; i++){
-            if(!table[i])System.out.println(i);
+        for(int j = 2; j < max+1; j++){
+            if(!table[j])primes.add(j);
+        }
+        int times = Integer.parseInt(bf.readLine());
+        for(int i = 0; i < times; i++){
+            int input = Integer.parseInt(bf.readLine());
+            int[] result = {0,10001};
+            outerloop:
+            for(int j = 0; primes.get(j) <= input; j++){
+                for(int k = 0;  primes.get(k) <= input; k++){
+                    int a = primes.get(j);
+                    int b = primes.get(k);
+                    if(a + b==input&& b-a >= 0 && result[1]-result[0] > b-a){
+                        result = new int[]{a, b};
+                        break;
+                    }
+                    if(k==primes.size()-1)break;
+                    if(a+b == input && b-a < 0)break outerloop;
+                }
+                if(j==primes.size()-1)break;
+            }
+            System.out.println(result[0] + " " + result[1]);
         }
     }
 }
