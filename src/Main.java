c3b9265ef83c432db7bf5426e1ec588bae943eeb
Jackson Hong <jacksonhong0111@gmail.com>
2021-01-12 18:15:09 +0900

    baekjoon #3053

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,27 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.math.BigDecimal;
 import java.util.*;
 
 public class Main {
     //baekjoon #1085
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer sb = new StringTokenizer(bf.readLine(), " ");
-        int x = Integer.parseInt(sb.nextToken());
-        int y = Integer.parseInt(sb.nextToken());
-        int w = Integer.parseInt(sb.nextToken());
-        int h = Integer.parseInt(sb.nextToken());
+        StringTokenizer st = new StringTokenizer(bf.readLine()," ");
+        int[] list = new int[3];
+        list[0] = Integer.parseInt(st.nextToken());
+        list[1] = Integer.parseInt(st.nextToken());
+        list[2] = Integer.parseInt(st.nextToken());
+        while(list[0]!=0&&list[1]!=0&&list[2]!=0){
+            list = Arrays.stream(list).sorted().toArray();
+            if(list[2]*list[2] == list[0]*list[0] + list[1]*list[1])System.out.println("right");
+            else System.out.println("wrong");
 
-        int distance = 1001;
-        if(distance > x) distance = x;
-        if(distance > y) distance = y;
-        if(distance > w-x) distance = w-x;
-        if(distance > h-y) distance = h-y;
-
-        System.out.println(distance);
-
+            StringTokenizer bb = new StringTokenizer(bf.readLine()," ");
+            list[0] = Integer.parseInt(bb.nextToken());
+            list[1] = Integer.parseInt(bb.nextToken());
+            list[2] = Integer.parseInt(bb.nextToken());
+        }
     }
 }
