9ffd3087ff0f939a5221bb2fdfe8205fef14d449
Jackson Hong <jacksonhong0111@gmail.com>
2021-01-13 17:51:43 +0900

    baekjoon #3053

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,16 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    //baekjoon #1085
+    //baekjoon #3053
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(bf.readLine()," ");
-        int[] list = new int[3];
-        list[0] = Integer.parseInt(st.nextToken());
-        list[1] = Integer.parseInt(st.nextToken());
-        list[2] = Integer.parseInt(st.nextToken());
-        while(list[0]!=0&&list[1]!=0&&list[2]!=0){
-            list = Arrays.stream(list).sorted().toArray();
-            if(list[2]*list[2] == list[0]*list[0] + list[1]*list[1])System.out.println("right");
-            else System.out.println("wrong");
+        int input = Integer.parseInt(bf.readLine());
 
-            StringTokenizer bb = new StringTokenizer(bf.readLine()," ");
-            list[0] = Integer.parseInt(bb.nextToken());
-            list[1] = Integer.parseInt(bb.nextToken());
-            list[2] = Integer.parseInt(bb.nextToken());
-        }
+        System.out.printf("%.6f \n",Math.PI*input*input);
+        System.out.printf("%.6f",(Math.pow(input*2,2))/2);
+
     }
 }
