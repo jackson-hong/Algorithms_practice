5fad09d9d06ad6c69675729ac030a3fe81754fc1
Jackson Hong <jacksonhong0111@gmail.com>
2021-01-14 17:10:51 +0900

    baekjoon #10872

--- src/Main.java
+++ src/Main.java
@@ -4,51 +4,18 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    //baekjoon #1002
+    //baekjoon #10872
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        int times = Integer.parseInt(bf.readLine());
+        int input = Integer.parseInt(bf.readLine());
+        System.out.println(recursion(input));
+    }
 
-        for(int i = 0; i < times; i++) {
-            StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
-            int[] joe = new int[3];
-            int[] lee = new int[3];
-            joe[0] = Integer.parseInt(st.nextToken());
-            joe[1] = Integer.parseInt(st.nextToken());
-            joe[2] = Integer.parseInt(st.nextToken());
-            lee[0] = Integer.parseInt(st.nextToken());
-            lee[1] = Integer.parseInt(st.nextToken());
-            lee[2] = Integer.parseInt(st.nextToken());
-            int bigR = 0;
-            int smallR = 0;
-            if(lee[2]>joe[2]){
-                bigR = lee[2];
-                smallR = joe[2];
-            }else {
-                bigR = joe[2];
-                smallR = lee[2];
-            }
-            double distance = Math.sqrt(Math.pow(joe[0]-lee[0],2)+Math.pow(joe[1]-lee[1],2));
-
-            if(distance==0){
-                if(lee[2]==joe[2]) {
-                    System.out.println(-1);
-                    continue;
-                }else{
-                    System.out.println(0);
-                    continue;
-                }
-            }
-
-            if(bigR > distance){
-                if(bigR - smallR < distance)System.out.println(2);
-                else if(bigR - smallR == distance)System.out.println(1);
-                else System.out.println(0);
-            }else {
-                if(bigR + smallR > distance)System.out.println(2);
-                else if(bigR + smallR == distance)System.out.println(1);
-                else System.out.println(0);
-            }
+    public static int recursion(int input){
+        if(input <= 1){
+            return 1;
+        }else {
+            return input * recursion(input-1);
         }
     }
 }
